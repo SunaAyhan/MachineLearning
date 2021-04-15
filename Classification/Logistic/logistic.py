ef6087ea6bb7994f78b711999c9d79652efcf0df
SunaAyhan <sunaayhan16@gmail.com>
2021-04-15 12:29:45 +0300

    classification

---
+++ Classification/Logistic/logistic.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#veri yukleme
+veriler = pd.read_csv('veriler.csv')
+
+x=veriler.iloc[:,1:4].values #bagimsiz degiskenler
+y=veriler.iloc[:,4:].values #bagimli degisken
+
+#egitim ve test bolmesi
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.33, random_state=0 )
+
+#egit ve uygula (fit / transform)
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+
+x_train = sc.fit_transform(x_train)
+x_test = sc.transform(x_test)
+
+#logistic regression
+from sklearn.linear_model import LogisticRegression
+logR = LogisticRegression(random_state=0)
+logR.fit(x_train,y_train)
+
+#tahmin ettir
+y_pred = logR.predict(x_test)
+print(y_pred)
+
+
+#tahmin ve gerçek değerler ne kadar uyumlu? (sagust dogru degerler sol alt yanlış değerler)
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test,y_pred)
+print(cm)
